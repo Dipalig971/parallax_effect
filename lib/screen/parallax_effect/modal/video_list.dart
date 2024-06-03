63ac7ad1702b12463419d263b33076c1bcbbacff
Dipalig971 <dipaligunjal971@gmail.com>
2024-06-03 14:59:15 +0530

    Initial commit

---
+++ lib/screen/parallax_effect/modal/video_list.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+final videoList=[
+  'assets/video/video1.mp4',
+  'assets/video/video2.mp4',
+  'assets/video/video3.mp4',
+  'assets/video/video4.mp4',
+  'assets/video/video5.mp4',
+];
